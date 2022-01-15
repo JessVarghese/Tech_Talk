93a01746c5499f46d7ae99a932bb106c355a4641
Jess Varghese <jessvarghese@gmail.com>
2022-01-14 22:45:08 -0500

    login fixed

--- seeds/post-seeds.js
+++ seeds/post-seeds.js
@@ -3,12 +3,12 @@ const { Post } = require('../models');
 const postData = [
   {
     title: '5 predictions for bitcoin, NFTs and the future of money',
-    post_url: 'https://www.cnet.com/personal-finance/crypto/5-predictions-for-bitcoin-nfts-and-the-future-of-money/',
+    content: 'https://www.cnet.com/personal-finance/crypto/5-predictions-for-bitcoin-nfts-and-the-future-of-money/',
     user_id: 1
   },
   {
     title: 'iPhone 14 design rumors: Buh-bye, notch; hello, hole and pill cutouts',
-    post_url: 'https://www.cnet.com/tech/mobile/iphone-14-design-rumors/',
+    content: 'https://www.cnet.com/tech/mobile/iphone-14-design-rumors/',
     user_id: 2
   }
 ];
