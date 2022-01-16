d076831d0761c76f31f5c01cc8b19989815d5444
Jess Varghese <jessvarghese@gmail.com>
2022-01-16 12:41:09 -0500

    updated main handlebars login file path

--- server.js
+++ server.js
@@ -1,33 +1,51 @@
-const express = require('express');
-const routes = require('./controllers');
-const sequelize = require('./config/connection');
+const router = require('express').Router();
+const apiRoutes = require('./controllers/api/');
+const homeRoutes = require('./controllers/home-routes');
+const dashboardRoutes = require('./controllers/dashboard-routes');
+router.use('/api', apiRoutes);
+router.use('/', homeRoutes);
+
 const path = require('path');
-const helpers = require('./utilis/helpers');
-const exphbs = require('express-handlebars');
-const hbs = exphbs.create({ helpers });
+const express = require('express');
 const session = require('express-session');
+
+const exphbs = require('express-handlebars');
+
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+const sequelize = require("./config/connection");
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
 const sess = {
-    secret: 'Super secret secret',
-    cookie: { maxAge: 36000 },
-    resave: false,
-    saveUninitialized: true,
-    store: new SequelizeStore({
-        db: sequelize
-    })
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
 };
+
 app.use(session(sess));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
+
+const helpers = require('./utilis/helpers');
+
+const hbs = exphbs.create({ helpers });
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
-app.use(routes);
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.listen(PORT, () => {
-  console.log(`Now Listening on ${ PORT }`);
+
+
+app.use(require('./controllers'));
+
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
+
 });
