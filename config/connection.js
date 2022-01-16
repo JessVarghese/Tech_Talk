c52f3e1969248c2d9a9c24b6a8d2a9175289d17d
Jess Varghese <jessvarghese@gmail.com>
2022-01-16 11:02:25 -0500

    updated connection js

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,7 @@ const Sequelize = require('sequelize');
 require('dotenv').config();
 
 // create connection to our db
-let sequelize;
+
 if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
