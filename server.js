2d90e2e685834994689b8225867aaf81213960bc
Jess Varghese <jessvarghese@gmail.com>
2022-01-16 00:14:09 -0500

    updated server

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const exphbs = require('express-handlebars');
 const hbs = exphbs.create({ helpers });
 const session = require('express-session');
 const app = express();
-const PORT = process.env.PORT || 3306;
+const PORT = process.env.PORT || 3001;
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const sess = {
     secret: 'Super secret secret',
